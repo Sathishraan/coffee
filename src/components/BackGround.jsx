252346e78c066fe0ad8f89daa2c00506c4781e99
Sathish P <146912497+Sathishraan@users.noreply.github.com>
2025-06-04 13:33:03 +0530

    Update BackGround.jsx

--- src/components/BackGround.jsx
+++ src/components/BackGround.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import bg from "../assets/bg-cafe.jpg"
 
 const BackGround = () => {
     return (
         <div className=' dark relative'>
 
-            <img src="assets/bg-cafe.jpg" alt="bg" />
+            <img src={bg} alt="bg" />
 
             <div className='absolute top-15 left-10 right-10'>
                 <Header />
